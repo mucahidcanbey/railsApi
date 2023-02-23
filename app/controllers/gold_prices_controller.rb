e6a8280cfeb74cfab700f01b4be7cc8f1da149e2
Mucahid Canbey <mucahid.canbey@gmail.com>
2023-02-23 09:37:01 +0300

    bootstrap özellikleri eklendi

--- app/controllers/gold_prices_controller.rb
+++ app/controllers/gold_prices_controller.rb
@@ -2,8 +2,13 @@
 class GoldPricesController < ApplicationController
   
   def index
+    # API'nin web adresi
     url = 'https://api.genelpara.com/embed/altin.json'
+
+    # HTTParty kütüphanesi kullanılarak GET isteği yapılıyo
     response = HTTParty.get(url)
+    
+     # Yanıt verileri ayrıştırılıyor ve @currencies değişkenine atanıyor
     @currencies = response.parsed_response
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'gold_prices/index'
+  
+  get 'gold_prices', to: 'gold_prices#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "home#index"
+   root "home#main"
 end
